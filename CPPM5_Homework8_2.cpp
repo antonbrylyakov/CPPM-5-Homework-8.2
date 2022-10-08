268455401e89d9b322a415db5a1638b0c30ce66d
Anton Brylyakov <yoll@rambler.ru>
2022-10-07 20:10:38 -0700

    Добавил другие фигуры

--- CPPM5_Homework8_2.cpp
+++ CPPM5_Homework8_2.cpp
@@ -17,14 +17,48 @@ int main()
 	setlocale(LC_ALL, "Russian");
 	try
 	{
-		Triangle f1(10, 20, 30, 100, 40, 40);
-		std::cout << f1.getFigureCreationReport() << std::endl;
+		Triangle f(10, 20, 30, 100, 40, 45);
+		std::cout << f.getFigureCreationReport() << std::endl;
+	}
+	catch (InvalidFigure& ex)
+	{
+		std::cout << ex.what() << std::endl;
+	}
 
-		IsoScelesTriangle f2(10, 20, 20, 84);
-		std::cout << f2.getFigureCreationReport() << std::endl;
+	try
+	{
+		IsoScelesTriangle f(10, 20, 20, 84);
+		std::cout << f.getFigureCreationReport() << std::endl;
+	}
+	catch (InvalidFigure& ex)
+	{
+		std::cout << ex.what() << std::endl;
+	}
 
-		Rhombus f3(10, 20, 15);
-		std::cout << f3.getFigureCreationReport() << std::endl;
+	try
+	{
+		Parallelogram f(20, 30, 70, 110);
+		std::cout << f.getFigureCreationReport() << std::endl;
+	}
+	catch (InvalidFigure& ex)
+	{
+		std::cout << ex.what() << std::endl;
+	}
+
+	try
+	{
+		Rhombus f(10, 20, 15);
+		std::cout << f.getFigureCreationReport() << std::endl;
+	}
+	catch (InvalidFigure& ex)
+	{
+		std::cout << ex.what() << std::endl;
+	}
+
+	try
+	{
+		Square f(40);
+		std::cout << f.getFigureCreationReport() << std::endl;
 	}
 	catch (InvalidFigure& ex)
 	{
